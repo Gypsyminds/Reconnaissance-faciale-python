00d8f8890950601ffdf8a8d16517736f3eb138d4
khouloud laajili <91345998+Gypsyminds@users.noreply.github.com>
2023-11-29 20:34:07 +0100

    Add files via upload

--- Captures-visage.py
+++ Captures-visage.py
@@ -6,6 +6,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 
+
 cam = cv2.VideoCapture(0)
 detector=cv2.CascadeClassifier('D:\\facedetect\\haarcascade_frontalface_default.xml')
 db_connection = mysql.connector.connect(
@@ -15,50 +16,50 @@ db_connection = mysql.connector.connect(
              database='africoding-db'
 )
 cursor = db_connection.cursor()
-cursor.execute("SELECT id FROM users ORDER BY id DESC LIMIT 1;")
+cursor.execute("SELECT id_condidat FROM user ORDER BY id_condidat DESC LIMIT 1;")
 last_id = cursor.fetchone()
 cursor.close()
 #print( last_id[0]) if last_id else None
-Id = last_id[0] + 1
+Id = last_id[0] +1
 
-def capture_image():
+#def capture_image():
     # Capture de l'image depuis la webcam
-    camera = cv2.VideoCapture(0)
-    _, image = camera.read()
-    camera.release()
-    return image
+   # camera = cv2.VideoCapture(0)
+   # _, image = camera.read()
+    #camera.release()
+    #return image
 
 
-def convert_image_to_binary(image):
+#def convert_image_to_binary(image):
     # Conversion de l'image en données binaires
-    retval, buffer = cv2.imencode('.jpg', image)
-    return buffer.tobytes()
-def save_image_to_database(image_data):
+   # retval, buffer = cv2.imencode('.jpg', image)
+  #  return buffer.tobytes()
+#def save_image_to_database(image_data):
     #id_condidat = data.get('id_condidat')
     #image = data.get('image')  
     # Connexion à la base de données MySQL
-    connection = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='root',
-        database='africoding-db'
-    )    
-    cursor = connection.cursor()    
+   # connection = mysql.connector.connect(
+       # host='localhost',
+       # user='root',
+       # password='root',
+       # database='africoding-db'
+   # )    
+   # cursor = connection.cursor()    
     # Requête SQL pour insérer l'image dans la table appropriée    
-    sql_query = "INSERT INTO users (id ,image) VALUES (%s,%s)"
-    cursor.execute(sql_query ,(Id,image_data))
+    #sql_query = "INSERT INTO users (id_condidat ,image) VALUES (%s,%s)"
+    #cursor.execute(sql_query ,(Id,image_data))
       # Valider la transaction
-    connection.commit()    
+    #connection.commit()    
     # Fermer la connexion
-    cursor.close()
-    connection.close()
-if __name__ == "__main__":
+    #cursor.close()
+    #connection.close()
+#if __name__ == "__main__":
     # Capturer l'image
-    image = capture_image()                    
+    #image = capture_image()                    
     # Convertir l'image en données binaires
-    image_data = convert_image_to_binary(image)   
+   # image_data = convert_image_to_binary(image)   
     # Enregistrer l'image dans la base de données
-    save_image_to_database(image_data)
+    #save_image_to_database(image_data)
     #product = {'name': name, 'description': description, 'price': price}
     
 @app.route('/ma_fonction', methods=['GET'])
@@ -83,11 +84,11 @@ def test():
         break
     # break if the sample number is morethan 20
     elif sampleNum>20:
-        return ("123")
+   # return ("123")
         break
     cam.release()
     cv2.destroyAllWindows()
-         
+    #return ("captures enregistrées");     
 if __name__ == '__main__':
     app.run()
 
